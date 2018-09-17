374244dfd53a273be3f8937bb9dd4a73d15955b1
J_C___ <jcsumlin@gmail.com>
2018-09-17 09:21:38 -0500

    worker update to fix version crashing

--- main.py
+++ main.py
@@ -2,19 +2,25 @@
 import os
 import time
 
+from pushover import init, Client
+
 import LocalBitcoin
-from notify import Notify
+
+init(os.environ['pushover_key'])
+pushover = Client(os.environ['pushover_client_key']).send_message("Hello!", title="Hello")
+
+
 
 # config = configparser.ConfigParser()
 # config.read('auth.ini')
 # api_key = str(config.get('auth', 'notify_my_android'))
-api_key = os.environ['notify_my_android']
+# api_key = os.environ['notify_my_android']
 hmac_auth_key = os.environ['hmac_auth_key']
 # hmac_auth_key = str(config.get('auth', 'hmac_auth_key'))  # REQUIRED
 hmac_auth_secret = os.environ['hmac_auth_secret']
 # hmac_auth_secret = str(config.get('auth', 'hmac_auth_secret'))  # REQUIRED
 
-n = Notify(api_key)
+# n = Notify(api_key)
 lc = LocalBitcoin.LocalBitcoin(hmac_auth_key, hmac_auth_secret, False)
 
 if not os.path.isfile("notifications_sent.txt"):
@@ -41,13 +47,15 @@ def main():
                 if str(message['msg'].encode('utf-8')) == '':
                     message_contents = 'Message Field Is Empty'
                     if 'attachment_url' in message:
-                        message_contents = message_contents + '\n' + message['attachment_url']
+                        pushover.send(message_contents, title=message['sender']['name'] + ' - ' + str(message['contact_id']), url=message['attachment_url'])
                 else:
                     if 'attachment_url' in message:
-                        message_contents = str(message['msg'].encode('utf-8')) + '\n' + message['attachment_url']
+                        message_contents = str(message['msg'].encode('utf-8'))
+                        pushover.send(message_contents, title=message['sender']['name'] + ' - ' + str(message['contact_id']), url=message['attachment_url'])
                     else:
                         message_contents = str(message['msg'].encode('utf-8'))
-                n.notify(str(message['sender']['name']) + ' - ' + str(message['contact_id']), message_contents)
+                        pushover.send(message_contents, title=message['sender']['name'] + ' - ' + str(message['contact_id']))
+                # n.notify(str(message['sender']['name']) + ' - ' + str(message['contact_id']), message_contents)
                 print("------\nMessage Sent\n")
             elif message_author == "J_C":
                 print("------\nMessage From Me")
